acdeb755f10617b123580ddfa8f4d371f38bce6e
Spiridon Madalina <78969330+Madalina0303@users.noreply.github.com>
2021-02-15 20:12:21 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
         // Madalina Spiridon B2
         System.out.println("Hello World !");
-        String []lang={"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
-        int n= (int) (Math.random()*2000000);
-        // Math.random =>  un nr de tip double de la 0.0 la 1.0... il  inmultim cu 2 mil- cat tine int
+        String[] lang={"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
+        int n= (int) (Math.random()*1_000_000);
+        // Math.random =>  un nr de tip double de la 0.0 la 1.0... il  inmultim cu 1 mil- cat tine int
         System.out.println(n);
         n=n*3;
         System.out.println(n);
@@ -17,10 +17,15 @@ public class Main {
         System.out.println(n);
 
         // cifra de control
-        int cc=n%9;
-        if(cc==0)
-            cc=9;
+         while(n>=10){
+        int s = 0;
+        while(n!=0){
+            s += n%10;
+            n /= 10;
+        }
+        n = s;
+    }
 
-        System.out.println("Willy-nilly, this semester I will learn " + lang[cc]);
+        System.out.println("Willy-nilly, this semester I will learn " + lang[n]);
     }
 }
