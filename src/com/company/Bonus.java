57adc180210c26d0b8be0b4b6fa3980ad2f265b0
Spiridon Madalina <78969330+Madalina0303@users.noreply.github.com>
2021-02-18 22:27:01 +0200

    Update Bonus.java

--- src/com/company/Bonus.java
+++ src/com/company/Bonus.java
@@ -14,16 +14,16 @@ public class Bonus {
             // nodurile sunt de la 0 la n-1 ... vom construi vector de tati
             System.out.println("n este " + n);
             int r = 0;
-            int[] t = new int[n + 1];
-            int[] frunza = new int[n + 1];
-            int[] nivel = new int[n + 1];
+            int[] t = new int[n];
+            int[] frunza = new int[n];
+            int[] nivel = new int[n];
             nivel[0] = 0;
             Arrays.fill(t, -1);
             Arrays.fill(frunza, 1);
             t[r] = 0;
             Bonus app = new Bonus();
             app.generare(n, t, frunza, nivel);
-            int[] viz = new int[n + 1];
+            int[] viz = new int[n];
             int[] ok = new int[1];
             app.constructie(n, t, frunza, nivel, r, viz, ok);
             Arrays.fill(viz, 0);
